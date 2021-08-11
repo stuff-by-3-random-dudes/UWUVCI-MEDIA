2a6d963d7735f65b4178c467d654b756c489fb86
NicoAICP <nicohaberkorn@gmx.de>
2021-08-11 23:11:09 +0200

    Added MediaFrame, will be used for mediainjection

---
+++ UWUVCI MEDIA/UI/Frames/MenuFrames/MediaFrame.xaml.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace UWUVCI_MEDIA.UI.Frames.MenuFrames
+{
+    public partial class MEDIAFRAME : Page, IDisposable
+    {
+     
+        public MEDIAFRAME()
+        {
+           
+            
+            InitializeComponent();
+           
+        }
+       
+        public void Dispose()
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
