78b9d59729c7f67be68e357fa0cab9d6107a0038
NicoAICP <nicohaberkorn@gmx.de>
2021-08-11 22:47:13 +0200

    Projektdateien hinzufügen.

---
+++ UWUVCI MEDIA/Models/BaseModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace UWUVCI_MEDIA.Models
+{
+    [Serializable]
+    public class BaseModel : INotifyPropertyChanged
+    {
+        // Declare the PropertyChanged event
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // OnPropertyChanged will raise the PropertyChanged event passing the
+        // source property that is being updated.
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ UWUVCI MEDIA/UI/Windows/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UWUVCI_MEDIA.UI.Windows
+{
+    /// <summary>
+    /// Interaktionslogik für MainWIndow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+        }
+        private void ButtonCloseMenu_Click(object sender, MouseEventArgs e)
+        {
+          
+
+        }
+
+        private void ButtonOpenMenu_Click(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            min.Background = new SolidColorBrush(Color.FromArgb(100, 255, 255, 255));
+        }
+        private void close_MouseEnter(object sender, MouseEventArgs e)
+        {
+            close.Background = new SolidColorBrush(Color.FromArgb(150, 255, 100, 100));
+        }
+
+        private void close_MouseLeave(object sender, MouseEventArgs e)
+        {
+            close.Background = new SolidColorBrush(Color.FromArgb(0, 250, 250, 250));
+        }
+
+        private void min_MouseLeave(object sender, MouseEventArgs e)
+        {
+            min.Background = new SolidColorBrush(Color.FromArgb(0, 250, 250, 250));
+        }
+        public void ListView_Click(object sender, MouseButtonEventArgs e)
+        {
+        }
+            private void Window_Close(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void MoveWindow(object sender, MouseButtonEventArgs e)
+        {
+            
+            try
+            {
+                if (e.ChangedButton == MouseButton.Left)
+                {
+                    
+                    DragMove();
+                }
+            }
+            catch (Exception)
+            {
+                //left empty on purpose
+            }
+           
+        }
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            new Task(() =>
+            {
+                System.Threading.Thread.Sleep(30);
+                
+            }).Start();
+        }
+        private void Window_Minimize(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+    }
+}
